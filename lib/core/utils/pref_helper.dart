5c75e505a5748ad0eacfee2deab360527a930c6f
rolarefaat456 <143338680+rolarefaat456@users.noreply.github.com>
2025-10-15 20:58:29 +0300

    pref helper

--- lib/core/utils/pref_helper.dart
+++ lib/core/utils/pref_helper.dart
@@ -1,1 +1,22 @@
 // fy el shared prefrences
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PrefHelper {
+  static const String tokenKey = 'auth_token';
+  static Future<void> saveToken( String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(tokenKey, token);
+  }
+  
+  static Future<String?> getToken( String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.getString(tokenKey);
+    return null;
+  }
+
+  static Future<void> clearToken( String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove(tokenKey);
+  }
+
+}
